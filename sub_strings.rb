39b98ac99234a304f56e7ead09dbb763d089db87
Yashar Zafari <yasharez@gmail.com>
2021-05-13 21:10:11 -0700

    Solved the challenge

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,13 +1,16 @@
 #sub string challenge
 
 def substrings(string, dictionary)
+  stringArr = string.split(' ')
   substring_count = Hash.new
-  dictionary.each do |word|
-    unless string.index(word)
-      unless substring_count[word]
-        substring_count[word] = 1
-      else
-        substring_count[word]++
+  stringArr.each do |array_item|
+    dictionary.each do |word|
+      if array_item.index(word)
+        unless substring_count[word]
+          substring_count[word] = 1
+        else
+          substring_count[word] += 1
+        end
       end
     end
   end
@@ -15,4 +18,5 @@ def substrings(string, dictionary)
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-p substrings('below', dictionary)
+# p substrings('below', dictionary)
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
